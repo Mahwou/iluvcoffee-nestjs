6270ec177bb51b1a39cf3c63376ba8b00bda1f5c
Mahwou <ndimbucourage@gmail.com>
2025-11-22 15:01:24 +0100

    refactor (app) refactor of delete

--- src/coffees/infrastructure/http/coffees.controller.ts
+++ src/coffees/infrastructure/http/coffees.controller.ts
@@ -1,10 +1,8 @@
 import { Controller} from '@nestjs/common';
-import { CoffeeService } from 'src/coffees/application/command/coffee.service';
 
 @Controller('coffees')
 export class CoffeesController {
 
     constructor(
-        private readonly coffeeService: CoffeeService,
     ) {}    
 }
